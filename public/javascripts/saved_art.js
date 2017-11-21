c021df9f41403353a72a77de6926869bdb488039
lacazeto <lucas.cazeto@gmail.com>
2017-11-21 09:21:43 +0100

    altering saved.ejs and .js

--- public/javascripts/saved_art.js
+++ public/javascripts/saved_art.js
@@ -1,10 +1,10 @@
 function addArticle(char) {
   document.getElementsByClassName("articles-container")[0].innerHTML += `
-  <div class="character-info">
-    <div class="name">ID: ${char.id}</div>
-    <div class="name">Name: ${char.name}</div>
-    <div class="occupation">Occupation: ${char.occupation}</div>
-    <div class="debt">Debt: ${char.debt}</div>
-    <div class="weapon">Weapon: ${char.weapon}</div>
+  <div class="article-info">
+    <div id="title">Title: ${myArticles.title}</div>
+    <div id="URL">URL: ${myArticles.URL}</div>
+    <div id="categories">Categories: ${myArticles.categories}</div>
+    <div id="location">Location: ${myArticles.location}</div>
+    <div id="date">Date: ${myArticles.date}</div>
   </div>`;
 }
