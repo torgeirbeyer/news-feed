6e4c2c15da664391cc64e0e2eeb6e64648ebc2cc
Torgeir Beyer <torgeir.beyer@gmail.com>
2017-11-21 14:09:57 +0100

    progress: easy css + log new position

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,76 +1,16 @@
 function main() {
   let map;
+  let markers = [];
+
   function startMap() {
     // CREATE MAP
-    const earth = {
-      lat: 0,
-      lng: 0
-    };
-    map = new google.maps.Map(document.getElementById("map"), {
+    let userLocation;
+    const mapOptions = {
       zoom: 15,
-      center: earth
-    });
-  }
+      center: userLocation
+    };
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-  function initializeSearch() {
-  // CREATE SEARCH INPUT
-    const input = document.getElementById("pac-input");
-    const searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-    map.addListener("bounds_changed", function() {
-      searchBox.setBounds(map.getBounds());
-    });
-
-    let markers = [];
-    searchBox.addListener("places_changed", function() {
-      const places = searchBox.getPlaces();
-
-      if (places.length === 0) {
-        return;
-      }
-
-      markers.forEach(function(marker) {
-        marker.setMap(null);
-      });
-      markers = [];
-
-      const bounds = new google.maps.LatLngBounds();
-      places.forEach(function(place) {
-        if (!place.geometry) {
-          console.log("Returned place contains no geometry");
-          return;
-        }
-        const icon = {
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25)
-        };
-
-        markers.push(
-          new google.maps.Marker({
-            map: map,
-            icon: icon,
-            title: place.name,
-            position: place.geometry.location
-          })
-        );
-
-        if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-          bounds.union(place.geometry.viewport);
-        } else {
-          bounds.extend(place.geometry.location);
-        }
-      });
-      map.fitBounds(bounds);
-    });
-  }
-
-  function initGeoLocation() {
-  // GET POSITION FROM IP
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         function(position) {
@@ -88,9 +28,10 @@ function main() {
               lat: userLocation.lat,
               lng: userLocation.lng
             },
-            map: map,
-            title: "You are here"
+            map: map
           });
+
+          console.log(userMarker.position.lat());
         },
         function() {
           console.log("Error in the geolocation service.");
@@ -100,9 +41,72 @@ function main() {
       console.log("Browser does not support geolocation.");
     }
   }
+
+  function initializeSearch() {
+  // CREATE SEARCH INPUT
+    const input = document.getElementById("pac-input");
+    const searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP].push(input);
+
+    map.addListener("bounds_changed", function() {
+      searchBox.setBounds(map.getBounds());
+    });
+
+    searchBox.addListener("places_changed", function() {
+      const places = searchBox.getPlaces();
+
+      if (places.length === 0) {
+        return;
+      }
+
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+        const latLng = marker.getPosition();
+
+        console.log(latLng);
+      });
+      markers = [];
+
+      const bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+
+        markers.push(
+          new google.maps.Marker({
+            map: map,
+            title: place.name,
+            position: place.geometry.location
+          })
+        );
+
+        const newLocation = {
+          lat: place.geometry.location.lat(),
+          lng: place.geometry.location.lng()
+        };
+
+        console.log(newLocation);
+
+        if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+  }
+
+  // function initGeoLocation() {
+  // GET POSITION FROM IP
+
+  // }
   startMap();
   initializeSearch();
-  initGeoLocation();
+  // initGeoLocation();
 }
 
 window.addEventListener("load", main);
