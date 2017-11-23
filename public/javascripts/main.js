867ed46220477e88f10270852d8b99f49af8c89d
Torgeir Beyer <torgeir.beyer@gmail.com>
2017-11-23 22:02:08 +0100

    fixing lucas computer

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,11 @@
-"use strict";
+// "use strict";
 
-const hamburger = document.getElementById("hamburger");
-const nav = document.getElementsByClassName("navlinks");
+// const hamburger = document.getElementById("hamburger");
+// const nav = document.getElementsByClassName("navlinks");
 
-hamburger.addEventListener("click", () => {
-  console.log(hamburger);
-  console.log(nav);
-  nav.classList.toggle("hidden");
-})
-;
+// hamburger.addEventListener("click", () => {
+//   console.log(hamburger);
+//   console.log(nav);
+//   nav.classList.toggle("hidden");
+// })
+// ;
